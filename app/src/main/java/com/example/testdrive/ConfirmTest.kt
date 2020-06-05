e12d738639b265105597a6ad2e9d30f6cb91f76d
vikassoni99 <vikassoni.soni22@gmail.com>
2020-06-05 17:30:59 +0530

    Initial Setup

---
+++ app/src/main/java/com/example/testdrive/ConfirmTest.kt
@@ -0,0 +1,11 @@
+package com.example.testdrive
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ConfirmTest:AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.confirm_test)
+    }
+}
---
+++ app/src/main/java/com/example/testdrive/ScoreActivity.kt
@@ -0,0 +1,12 @@
+package com.example.testdrive
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ScoreActivity:AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_score)
+
+    }
+}
